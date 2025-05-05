44b74b594f3f82c84be1ba0146fe26edca1328fd
dgarg7599 <gargdivyansh7599@gmail.com>
2025-05-05 13:33:38 +0530

    [Divyansh Garg] ADD: Done some modifications in Analytics Controller

--- src/main/java/com/cg/siptracker/controller/AnalyticsController.java
+++ src/main/java/com/cg/siptracker/controller/AnalyticsController.java
@@ -1,12 +1,15 @@
 package com.cg.siptracker.controller;
 
 import com.cg.siptracker.dto.SipSummaryDto;
+import com.cg.siptracker.exception.ResourceNotFoundException;
 import com.cg.siptracker.model.SIP;
 import com.cg.siptracker.repository.SIPRepository;
 import com.cg.siptracker.service.IAnalyticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
 
 
 @RestController
@@ -22,10 +25,19 @@ public class AnalyticsController {
     // Get SIP performance summary by SIP ID
     @GetMapping("/sip/{id}/summary")
     public ResponseEntity<SipSummaryDto> getSipSummary(@PathVariable Long id) {
+        // Extract logged-in user's email
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // Fetch the SIP and verify ownership
         SIP sip = sipRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("SIP not found with id: " + id));
+
+        if (!sip.getUser().getEmail().equals(email)) {
+            throw new ResourceNotFoundException("You are not authorized to access this SIP");
+        }
 
         SipSummaryDto summary = IAnalyticsService.analyzeSIP(sip);
         return ResponseEntity.ok(summary);
     }
+
 }
